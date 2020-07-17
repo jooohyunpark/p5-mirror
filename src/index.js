2e345dc8d68f973f6e94ffac1505bf65c809596e
Joohyun Park <jhp527@nyu.edu>
2020-07-16 23:37:33 -0400

    mirror rect wip

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import './assets/style/main.scss'
 import * as p5 from 'p5'
 
-// image data processing
 const mirror = _p5 => {
   const size = 300
   const width = size * 4
   const height = size * 3
-  const interval = 50
+  const interval = 30
 
   let img
 
@@ -14,6 +13,8 @@ const mirror = _p5 => {
     _p5.frameRate(30)
     _p5.createCanvas(width, height)
     _p5.noStroke()
+    _p5.rectMode(_p5.CENTER)
+    // _p5.blendMode(_p5.EXCLUSION)
 
     img = _p5.createCapture(_p5.VIDEO)
     img.size(width, height)
@@ -27,18 +28,20 @@ const mirror = _p5 => {
     for (let y = 0; y < img.height; y += interval) {
       for (let x = 0; x < img.width; x += interval) {
         const i = y * width + (img.width - x - 1)
-        const r = (255 - img.pixels[i * 4]) / 255
-        const g = (255 - img.pixels[i * 4 + 1]) / 255
-        const b = (255 - img.pixels[i * 4 + 2]) / 255
-        const darkness = (r + g + b) / 3
-        const radius = (interval / 2) * Math.pow(darkness, 2)
-        // const r = img.pixels[i * 4] / 255
-        // const g = img.pixels[i * 4 + 1] / 255
-        // const b = img.pixels[i * 4 + 2] / 255
-        // const brightness = (r + g + b) / 3
-        // const radius = interval * brightness
-        _p5.fill(255)
-        _p5.ellipse(x + interval / 2, y + interval / 2, radius, radius)
+        const r = img.pixels[i * 4]
+        const g = img.pixels[i * 4 + 1]
+        const b = img.pixels[i * 4 + 2]
+        const darkness = (r + g + b) / (255 * 3)
+        const radius = interval * 1 * Math.pow(darkness, 2)
+
+        const max = Math.max(r, g, b)
+        const rr = r === max ? max : 0
+        const gg = g === max ? max : 0
+        const bb = b === max ? max : 0
+
+        _p5.fill(r, g, b)
+        _p5.rect(x + interval / 2, y + interval / 2, radius, radius)
+        // _p5.ellipse(x + interval / 2, y + interval / 2, radius, radius)
       }
     }
   }
