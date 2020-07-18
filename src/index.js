5e2674b06c8553ef68d898ec071e081e1a562b63
Joohyun Park <jhp527@nyu.edu>
2020-07-17 21:47:57 -0400

    update

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import * as p5 from 'p5'
 
 const params = new URLSearchParams(window.location.search)
 
-const size = Number(params.get('size')) || 300
+const size = Number(params.get('s')) || 300
 const width = size * 4
 const height = size * 3
-const radius = Number(params.get('radius')) || 30
+const radius = Number(params.get('r')) || 30
 
 const mirror = _p5 => {
   let img
 
   _p5.setup = () => {
-    _p5.frameRate(20)
     _p5.createCanvas(width, height)
+    _p5.frameRate(30)
     _p5.noStroke()
     _p5.rectMode(_p5.CENTER)
     // _p5.blendMode(_p5.EXCLUSION)
@@ -35,12 +35,10 @@ const mirror = _p5 => {
         const b = img.pixels[i * 4 + 2]
         const darkness = 1 - (r + g + b) / (255 * 3)
         const _radius = radius * 1 * Math.pow(darkness, 2)
-
         const color = _p5.map(darkness, 0, 1, 0, 255)
 
         _p5.fill(color)
         _p5.rect(x + radius / 2, y + radius / 2, _radius, _radius)
-        // _p5.ellipse(x + interval / 2, y + interval / 2, radius, radius)
       }
     }
   }
