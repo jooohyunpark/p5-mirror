a8389336903fa6595d2d879fedcc8a057e4273c0
Joohyun Park <jhp527@nyu.edu>
2020-07-17 21:07:01 -0400

    add query

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import './assets/style/main.scss'
 import * as p5 from 'p5'
 
-const mirror = _p5 => {
-  const size = 300
-  const width = size * 4
-  const height = size * 3
-  const interval = 30
+const params = new URLSearchParams(window.location.search)
 
+const size = Number(params.get('size')) || 300
+const width = size * 4
+const height = size * 3
+const radius = Number(params.get('radius')) || 30
+
+const mirror = _p5 => {
   let img
 
   _p5.setup = () => {
@@ -25,19 +27,19 @@ const mirror = _p5 => {
     _p5.clear()
     img.loadPixels()
 
-    for (let y = 0; y < img.height; y += interval) {
-      for (let x = 0; x < img.width; x += interval) {
+    for (let y = 0; y < img.height; y += radius) {
+      for (let x = 0; x < img.width; x += radius) {
         const i = y * width + (img.width - x - 1)
         const r = img.pixels[i * 4]
         const g = img.pixels[i * 4 + 1]
         const b = img.pixels[i * 4 + 2]
         const darkness = 1 - (r + g + b) / (255 * 3)
-        const radius = interval * 1 * Math.pow(darkness, 2)
+        const _radius = radius * 1 * Math.pow(darkness, 2)
 
         const color = _p5.map(darkness, 0, 1, 0, 255)
 
         _p5.fill(color)
-        _p5.rect(x + interval / 2, y + interval / 2, radius, radius)
+        _p5.rect(x + radius / 2, y + radius / 2, _radius, _radius)
         // _p5.ellipse(x + interval / 2, y + interval / 2, radius, radius)
       }
     }
